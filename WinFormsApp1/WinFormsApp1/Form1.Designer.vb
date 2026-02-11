9b99bfe027b3ce79b472ed2571cdb20bd85619c0
Leenlnza <niwat3706@gmail.com>
2026-02-11 09:31:35 +0700

    srg

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        DataGridView1 = New DataGridView()
+        TextBox2 = New TextBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(338, 77)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(321, 45)
+        Label1.TabIndex = 0
+        Label1.Text = "โปรแกรมคำนวนภาษี 7%"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(338, 157)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(310, 39)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(786, 148)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(129, 61)
+        Button1.TabIndex = 2
+        Button1.Text = "คำนวณ"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = SystemColors.Window
+        DataGridViewCellStyle1.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle1.ForeColor = SystemColors.ControlText
+        DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = DataGridViewTriState.False
+        DataGridView1.DefaultCellStyle = DataGridViewCellStyle1
+        DataGridView1.Location = New Point(127, 238)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(788, 297)
+        DataGridView1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.Location = New Point(127, 157)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(188, 39)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1016, 644)
+        Controls.Add(TextBox2)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox2 As TextBox
+
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.DirectoryServices.ActiveDirectory
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.Columns.Add("Name", "ชื่อสินค้า")
+        DataGridView1.Columns.Add("price", "ราคา")
+        DataGridView1.Columns.Add("vat", "ภาษี")
+        DataGridView1.Columns.Add("total", "ราคาสุทธิ")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If String.IsNullOrEmpty(TextBox1.Text) Or Not IsNumeric(TextBox1.Text) Then
+            MessageBox.Show("กรุณาใส่ราคาให้ถูกต้อง", "ระวัง", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            TextBox1.Clear()
+            TextBox1.Focus()
+            Return
+        End If
+
+        Dim price As Decimal = TextBox1.Text
+        Dim vat As Decimal = price * 0.07
+        Dim total As Decimal = price + vat
+        Dim ttt As Decimal = 0
+        DataGridView1.Rows.Add(TextBox2.Text, price.ToString("N2"), vat.ToString("N2"), total.ToString("N2"))
+        TextBox2.Clear()
+        TextBox1.Clear()
+        TextBox2.Focus()
+    End Sub
+End Class
